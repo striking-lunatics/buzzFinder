3a79045edb4e6dd0f914833fb0e8ea7c62040ef7
Dillon Lundell <dillonjaylundell@gmail.com>
2016-07-08 13:11:49 -0500

    login/signup buttons. Auth component rendered in button;

--- client/components/AuthPanel.jsx
+++ client/components/AuthPanel.jsx
@@ -6,14 +6,30 @@ export default class AuthPanel extends React.Component {
 
    constructor(props) {
       super(props);
-      this.state = {};
+      this.state = {username:'', password:''};
    }
 
    render() {
       return (
         <div>
+        <form>
+          <input
+          value={this.state.username}
+          placeholder="Username..."
+          onChange={(e) => this.setState({username:e.target.value})} 
+           />
+          <input
+          value={this.state.password}
+          placeholder='Password...'
+          onChange={(e) => this.setState({password:e.target.value})}
+           />
+           uN:{this.state.username}
+           pW:{this.state.password}
 
+          <button>Submit</button>
+          </form>
         </div>
+
       );
    }
 }
