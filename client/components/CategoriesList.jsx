ae7cb84ca2bb3cb9731164e0142ea75dc2ef3744
Christopher Wiles <chriswiles87@gmail.com>
2016-07-08 08:07:07 -0500

    added basic components outlines

---
+++ client/components/CategoriesList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class CategoriesList extends React.Component {
+
+   constructor(props) {
+      super(props);
+      this.state = {};
+   }
+
+   render() {
+      return ();
+   }
+}
