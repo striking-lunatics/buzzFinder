155f92ae132fe423526124f48321037e1372aabe
Christopher Wiles <chriswiles87@gmail.com>
2016-07-08 18:30:03 -0500

    send location to server

---
+++ BreweryDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class BreweryDetail extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
---
+++ index.jsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BreweryItem from './components/BreweryItem.jsx';
+import fetch from 'isomorphic-fetch';
+import $ from 'jquery';
+
+//import {Button,Modal,Popover,} from 'react-bootstrap';
+// import {Button, Modal} from "react-bootstrap";
+import AuthButton from './components/AuthButton.jsx'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: '',
+            breweryList: [],
+            latitude: 0,
+            longitude: 0
+        }
+
+    }
+
+    _getLocation() {
+        // get location from client Browser (HTML5 funciton)
+        if ('geolocation' in navigator) {
+            this._requestLocation();
+        } else {
+            console.log("Browser doesn't support geolocation");
+        }
+    }
+
+    _requestLocation() {
+        const options = {
+            enableHighAccuracy: false,
+            timeout: 5000,
+            maximumAge: 0
+        };
+        // call HTML5 funciton, passing the coords object (this) to _success()
+        navigator.geolocation.getCurrentPosition(this._success.bind(this), error, options);
+        const error = (err) => console.log('error ', err);
+    }
+
+    _success(pos) {
+        this.setState({latitude: pos.coords.latitude, longitude: pos.coords.longitude})
+        console.log('Lat', this.state.latitude, 'long', this.state.longitude)
+
+        // send coords obj to server
+        // server makes api call for brewery by location
+        $.ajax({
+            url: '/location',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                latitude: this.state.latitude,
+                longitude: this.state.longitude
+            }),
+            dataType: 'json',
+            success: this._fetchBrewerysByLocation.bind(this)
+        });
+        // fetch('http://localhost:8080/location', {
+        //     method: "POST",
+        //     body: JSON.stringify({latitude: this.state.latitude, longitude: this.state.longitude}),
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     },
+        //     credentials: "same-origin"
+        // }).then(function(response) {
+        //     console.log(response);
+        //
+        //     response.text().then(function(responseText) {})
+        // }, function(error) {
+        //     error.message //=> String
+        // })
+    }
+
+
+
+    // setState with brewery data
+    _fetchBrewerysByLocation(brewerys) {
+        this.setState({breweryList: brewerys.data});
+        // we now can use this data in the child components
+        // this.props.breweryList
+    }
+
+    render() {
+        return (
+            <div className="container App">
+                <div className="row">
+                    <BreweryItem/>
+                    <AuthButton/>
+                    <div className="col-sm-offset-2 col-sm-8">
+                        {/*{ Create search box and add two-way bindings }*/}
+                        <form>
+                            <input value={this.state.term} onKeyPress={(e) => this.test(e)} onChange={(e) => this.setState({term: e.target.value})} type='text' className='form-control' placeholder='Search by city.....'/>
+                            <button onClick={(e) => e.preventDefault()}>Search</button>
+                            here is our state: {this.state.term}
+                        </form>
+                        <button onClick={() => this._getLocation()}>Use current location</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    test(e) {
+        if (e.key === 'Enter')
+            alert('they pressed enter we will search by provided city');
+        }
+    }
+
+ReactDOM.render(
+    <App/>, document.getElementById('root'));
