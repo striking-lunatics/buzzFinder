7749ce9f73a632ff44d48e673baf8f40f3b78dc2
Christopher Wiles <chriswiles87@gmail.com>
2016-07-11 16:05:30 -0500

    brewerys is not defined

--- client/components/BreweryList.jsx
+++ client/components/BreweryList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import BreweryItem from './BreweryItem.jsx';
+import $ from 'jquery';
 //import App from './App.jsx';
 const request = require('request');
 
@@ -24,39 +25,32 @@ export default class BreweryList extends React.Component {
    }
 
    _fetchLocation() {
+     const self = this;
       request('http://ip-api.com/json', function(error, response, body) {
          if (!error && response.statusCode == 200) {
             const IP = JSON.parse(body);
+
             console.log('BreweryList -> _fetchLocation', IP.lat, IP.lon);
-            //this.setState({latitude: IP.lat, longitude: IP.lon});
-            //this._fetchBrwerysLocation(IP.lat, IP.lon);
-            const API = 'da506aecce47e548b1877f8c6f9be793';
-            const URL = `http://api.brewerydb.com/v2/search/geo/point?radius=25&lat=${ IP.lat}&lng=${IP.lon}&key=${API}`;
-            request(URL, function(error, response, body) {
-              //Fetch API cannot load http://api.brewerydb.com/v2/search/geo/point?radius=25&lat=30.4998&lng=-97.8082&key=da506aecce47e548b1877f8c6f9be793.
-              // No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'http://localhost:1337' is therefore not allowed access.
-              //  If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
-               if (!error && response.statusCode == 200) {
-                  const brewerys = JSON.parse(body).data;
-                  this.setState({brewerys: data});
-                  console.log('_fetchBrwerysLocation', this.state.brewerys)
-               }
-            })
+
+            self._success(IP);
+
          }
       })
    }
 
-  //  _fetchBrwerysLocation(lat, long) {
-  //     const API = 'da506aecce47e548b1877f8c6f9be793';
-  //     const URL = `http://api.brewerydb.com/v2/search/geo/point?radius=25&lat=${lat}&lng=${long}&key=${API}`;
-  //     request(URL, function(error, response, body) {
-  //        if (!error && response.statusCode == 200) {
-  //           const brewerys = JSON.parse(body).data;
-  //           this.setState({brewerys: data});
-  //           console.log('_fetchBrwerysLocation', this.state.brewerys)
-  //        }
-  //     })
-  //  }
+
+
+   _success(IP) {
+     const s = this;
+     $.ajax({
+        url: '/location',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({latitude: IP.lat, longitude: IP.lon}),
+        dataType: 'json',
+        success: (data) => s.setState({brewerys: brewerys.data})
+     });
+   }
 
    _createBreweryComponents() {
       if (this.state.brewerys) {
