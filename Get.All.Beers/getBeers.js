8a7e2442d9ab099ee0da4f3faa95e8f5e49aae3a
Christopher Wiles <chriswiles87@gmail.com>
2016-07-11 12:57:52 -0500

    added  getallbeers server example

---
+++ Get.All.Beers/getBeers.js
@@ -0,0 +1,34 @@
+const request = require('request');
+const API = 'da506aecce47e548b1877f8c6f9be793'
+fetchLocation()
+
+function fetchLocation() {
+   request('http://ip-api.com/json', function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+         const IP = JSON.parse(body);
+         console.log(IP.lat, IP.lon);
+         fetchBrwerysLocation(IP.lat, IP.lon)
+      }
+   })
+}
+
+function fetchBrwerysLocation(lat, long) {
+   const URL = `http://api.brewerydb.com/v2/search/geo/point?radius=25&lat=${lat}&lng=${long}&key=${API}`;
+   request(URL, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+         const brewerys = JSON.parse(body).data;
+         const brewerysIDs = brewerys.map(brewery => brewery.id);
+         brewerysIDs.forEach(id => fetchBeers(id));
+      }
+   })
+}
+
+function fetchBeers(id) {
+   const URL = `http://api.brewerydb.com/v2/brewery/${id}/beers?&key=${API}`;
+   request(URL, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+         const beers = JSON.parse(body).data;
+
+      }
+   })
+}
