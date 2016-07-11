8864a15196ac119913e3ee48c6cad0aae7fb2496
Christopher Wiles <chriswiles87@gmail.com>
2016-07-11 16:15:12 -0500

    added darkly.css

--- server/main.js
+++ server/main.js
@@ -10,43 +10,46 @@ app.use(express.static(path.join(__dirname, "../client/public")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
- extended: true
+   extended: true
 }));
 
 app.get('/app-bundle.js',
- browserify('./client/main.js', {
-    transform: [ [ require('babelify'), { presets: ["es2015", "react"] } ] ]
-  })
+   browserify('./client/main.js', {
+      transform: [
+         [require('babelify'), {
+            presets: ["es2015", "react"]
+         }]
+      ]
+   })
 );
 
 // Additional middleware which will set headers that we need on each request.
 app.use(function(req, res, next) {
- // Set permissive CORS header - this allows this server to be used only as
- // an API server in conjunction with something like webpack-dev-server.
- res.setHeader('Access-Control-Allow-Origin', '*');
+   // Set permissive CORS header - this allows this server to be used only as
+   // an API server in conjunction with something like webpack-dev-server.
+   res.setHeader('Access-Control-Allow-Origin', '*');
 
- // Disable caching so we'll always get the latest comments.
- res.setHeader('Cache-Control', 'no-cache');
- next();
+   // Disable caching so we'll always get the latest comments.
+   res.setHeader('Cache-Control', 'no-cache');
+   next();
 });
 
 app.post('/location', function(req, res) {
 
- console.log("runnnning")
- console.log("data:", req.body)
- console.log(req.body.latitude, req.body.longitude)
+   console.log("runnnning")
+      //console.log("data:", req.body)
+   console.log(req.body.latitude, req.body.longitude)
 
- const URL = `http://api.brewerydb.com/v2/search/geo/point?radius=100&lat=${req.body.latitude}&lng=${req.body.longitude}&key=${API}`;
+   const URL = `http://api.brewerydb.com/v2/search/geo/point?radius=100&lat=${req.body.latitude}&lng=${req.body.longitude}&key=${API}`;
 
-
-
- request(URL, function(error, response, body) {
-   if (!error && response.statusCode == 200) {
-     res.send(JSON.parse(body));
-   } else {
-   	console.log("error: ", error)
-   }
- })
+   request(URL, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+         console.log('Server: Sending Data')
+         res.send(JSON.parse(body));
+      } else {
+         console.log("error: ", error)
+      }
+   })
 });
 
 var port = process.env.PORT || 1337;
