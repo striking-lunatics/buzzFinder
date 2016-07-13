70d777a49c44967da0834685ef8127d5db03c194
Christopher Wiles <chriswiles87@gmail.com>
2016-07-13 11:55:35 -0500

    set up beer list

--- client/components/BeerList.jsx
+++ client/components/BeerList.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import BeerItem from './BreweryItem.jsx';
 
 export default class BeerList extends React.Component {
-  render() {
-    <div>
 
-    </div>
-  }
+   _createBeerComponents() {
+      if (this.props.beers) {
+         return this.props.beers.map((beer, index) => {
+            return <BeerItem
+              key={index}
+              name={beer.name}
+              id={beer.id}
+              description={beer.description}
+              abv={beer.abv}
+              ibu={beer.ibu}
+              style={beer.style.category.name}/>
+         });
+      }  else {
+         return <h2>Loading...</h2>
+      }
+   }
+
+   render() {
+      return (
+         <div className='beerList'>
+            {this._createBeerComponents()}
+         </div>
+      );
+   }
 }
