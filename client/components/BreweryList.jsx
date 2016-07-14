a22413c106c46fff6c1b95156c5a4275d7bac6ca
sxcw <yuan329419@gmail.com>
2016-07-14 14:46:18 -0500

    Add next/prev without beerlist

--- client/components/BreweryList.jsx
+++ client/components/BreweryList.jsx
@@ -1,25 +1,86 @@
 import React from 'react';
 import BreweryItem from './BreweryItem.jsx';
+import BreweryView from './BreweryView.jsx';
 import $ from 'jquery';
 import Loader from './loadingComponent.jsx';
 const request = require('request');
 
 export default class BreweryList extends React.Component {
 
+   constructor(props) {
+     super(props);
+     this.state = {
+       activeId: undefined
+     }
+   }
+
    render() {
+
+      const activeId = this.state.activeId;
+      let company;
+      if (activeId === undefined) {
+        company = undefined;
+      } else {
+        company = this.props.brewerys[activeId];
+        console.log('company is~~~~' , company)
+      }
+
+      let breweryView;
+      if (company === undefined) {
+        breweryView = null;
+      } else {
+        breweryView = <BreweryView
+                company={company}
+                closeView={this.closeView.bind(this)} 
+                nextView={this.nextView.bind(this)} 
+                prevView={this.prevView.bind(this)}
+              />;
+      }
+
       return (
+
          <div className='breweryList'>
             {this._createBreweryComponents()}
+            {console.log(breweryView)}
+            {breweryView}
          </div>
       )
    }
 
+   closeView() {
+     this.setState({
+       activeId: undefined
+     });
+     $("body").removeClass("modal-open")
+   }
+
+   nextView() {
+     this.setState({
+       activeId: this.state.activeId + 1
+     })
+   }
+
+   prevView() {
+     this.setState({
+       activeId: this.state.activeId - 1
+     })
+   }
+
+   selectView(activeViewId) {
+     // console.log('handling change' + activeViewId);
+     this.setState({
+       activeId: activeViewId
+     });
+   }
+
    _createBreweryComponents() {
       //  map brewerys data -> create BreweryItem for each brewery
+      console.log('this.props.brewerys~~~~',this.props.brewerys)
       if (this.props.brewerys) {
-         return this.props.brewerys.filter((beer) => beer.streetAddress && beer.openToPublic == "Y" && beer.locationType != "office" && beer.brewery.images).map((beer, index) => {
+         return this.props.brewerys.filter((beer) => beer.streetAddress && beer.openToPublic == "Y" && beer.locationType != "office" && beer.brewery.images && beer.brewery.images.squareMedium).map((beer, index) => {
             return <BreweryItem
               key={index}
+              viewId={index}
               breweryID={beer.id}
               url={beer.brewery.website}
               name={beer.brewery.name}
@@ -30,7 +91,8 @@ export default class BreweryList extends React.Component {
               type={beer.locationType}
               icon={beer.brewery.images.icon}
               image={beer.brewery.images.squareMedium}
-              socialAccounts={beer.brewery.socialAccounts}/>
+              socialAccounts={beer.brewery.socialAccounts}
+              selectView={this.selectView.bind(this)}/>
          });
       } else {
          return <Loader />
---
+++ client/components/breweryView.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {Button, Modal} from 'react-bootstrap';
+import BeerList from './BeerList.jsx';
+
+export default class BreweryView extends React.Component {
+
+   render() {
+
+      const show = (this.props.company !== undefined);
+      var self = this;
+      //console.log('rendering...');
+      //console.log(this.props.company);
+
+      return (
+         <div>
+            <Modal className='breweryDetail' show={show} onHide={this.props.closeView} container={this} aria-labelledby="contained-modal-title">
+               {/* Detail panel */}
+               <Modal.Header closeButton>
+                  <Modal.Title id="contained-modal-title"> <span className='title'> {this.props.company.brewery.name} </span>
+                    <span className='address'>{!(this.props.company.address)? null: this.props.company.address}</span>
+                  </Modal.Title>
+               </Modal.Header>
+               <Modal.Body>
+               {console.log('this.props.company~~~~~', this.props.company)}
+                  <a href={this.props.company.brewery.url} target='_blank'>
+                     <img className='center-block img-circle' 
+                     	src={this.props.company.brewery.images? this.props.company.brewery.images.squareMedium : 'http://www.frenchtoastsunday.com/wp-content/uploads/2015/02/beer-icon.png'} alt={this.props.company.brewery.name}/>
+                     
+                  </a>
+                  <br/>
+                  <div className='description'>
+                    {this.props.company.brewery.description || 'No description'}
+                  </div>
+
+                  {/* <BeerList breweryID={this.props.id}/> */}
+
+                  {/*<BeerList beers={self.state.beers}/>*/}
+
+               </Modal.Body>
+               <Modal.Footer>
+                  <div className='btn-group' role='group'>
+                     <button type='button' className='btn btn-primary' onClick={this.props.prevView}>
+                        &larr; Prev
+                     </button>
+                     <button type='button' className='btn btn-primary heart' onClick={(e) => {
+                        e.preventDefault()}}>
+                        &hearts;
+                     </button>
+                     {/*here is our state (test): {this.props.city}*/}
+                     <button type='button' className='btn btn-primary' onClick={this.props.nextView}>
+                        Next &rarr;
+                     </button>
+                  </div>
+               </Modal.Footer>
+            </Modal>
+         </div>
+      );
+   }
+}
