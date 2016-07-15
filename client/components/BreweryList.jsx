fee5249f36a81124bbe1ea3fd04c5826c3f0c4d6
sxcw <yuan329419@gmail.com>
2016-07-14 23:06:02 -0500

    Finish prev/next

--- client/components/BreweryList.jsx
+++ client/components/BreweryList.jsx
@@ -15,6 +15,7 @@ export default class BreweryList extends React.Component {
    }
 
    render() {
+    // console.log(this.props.brewerys);
 
       const activeId = this.state.activeId;
       let company;
@@ -22,13 +23,12 @@ export default class BreweryList extends React.Component {
         company = undefined;
       } else {
         company = this.props.brewerys[activeId];
-        console.log('this.props.brewerys~~', this.props.brewerys)
-        console.log('activeId is~~~~' , activeId)
       }
 
       let breweryView;
       if (company === undefined) {
         breweryView = null;
+        $("body").removeClass("modal-open");
       } else {
         breweryView = <BreweryView
                 company={company}
@@ -37,14 +37,37 @@ export default class BreweryList extends React.Component {
                 prevView={this.prevView.bind(this)}
               />;
       }
-
+    
       return (
-
          <div className='breweryList'>
-            {this._createBreweryComponents()}
+            {this._createBreweryView()}
             {breweryView}
          </div>
       )
+   }
+
+   _createBreweryView() {
+    if (this.props.brewerys) {
+      return this.props.brewerys.map((beer, index) => {
+        return <BreweryItem
+          key={index}
+          viewId={index}
+          breweryID={beer.id}
+          url={beer.brewery.website}
+          name={beer.brewery.name}
+          description={beer.brewery.description}
+          address={beer.streetAddress}
+          zipcode={beer.postalCode}
+          distance={beer.distance}
+          type={beer.locationType}
+          icon={beer.brewery.images.icon}
+          image={beer.brewery.images.squareMedium}
+          socialAccounts={beer.brewery.socialAccounts}
+          selectView={this.selectView.bind(this)}/>
+      });
+    } else {
+      return <Loader />
+    }
    }
 
    closeView() {
@@ -67,34 +90,8 @@ export default class BreweryList extends React.Component {
    }
 
    selectView(activeViewId) {
-     // console.log('handling change' + activeViewId);
      this.setState({
        activeId: activeViewId
      });
-   }
-
-   _createBreweryComponents() {
-      //  map brewerys data -> create BreweryItem for each brewery
-      if (this.props.brewerys) {
-         return this.props.brewerys.filter((beer) => beer.streetAddress && beer.openToPublic == "Y" && beer.locationType != "office" && beer.brewery.images && beer.brewery.images.squareMedium).map((beer, index) => {
-            return <BreweryItem
-              key={index}
-              viewId={index}
-              breweryID={beer.id}
-              url={beer.brewery.website}
-              name={beer.brewery.name}
-              description={beer.brewery.description}
-              address={beer.streetAddress}
-              zipcode={beer.postalCode}
-              distance={beer.distance}
-              type={beer.locationType}
-              icon={beer.brewery.images.icon}
-              image={beer.brewery.images.squareMedium}
-              socialAccounts={beer.brewery.socialAccounts}
-              selectView={this.selectView.bind(this)}/>
-         });
-      } else {
-         return <Loader />
-      }
    }
 }
