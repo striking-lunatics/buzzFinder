16f5f031cff606fc4762b320983c8da8a3e8cb63
Christopher Wiles <chriswiles87@gmail.com>
2016-07-18 09:12:17 -0500

    formating

--- server/main.js
+++ server/main.js
@@ -35,7 +35,7 @@ function deg2rad(deg) {
 app.use(express.static(path.join(__dirname, "../client/public")));
 
 app.use(bodyParser.json());
-app.use(cookieParser() );
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({
    extended: true
 }));
@@ -72,27 +72,26 @@ app.post('/location', function(req, res) {
    const URL = `http://api.brewerydb.com/v2/search/geo/point?withSocialAccounts=Y&radius=30&lat=${req.body.latitude}&lng=${req.body.longitude}&key=${API}`;
 
    request(URL, function(error, response, body) {
-      if (!error && response.statusCode == 200) { 
+      if (!error && response.statusCode == 200) {
 
-        var parsedBody = JSON.parse(body);
-        // console.log("showing results for nearby radius:", parsedBody.data); 
-        // console.log()
-        includeBreweryLikes(parsedBody.data)
-          .then(function(breweriesWithLikes) {
-            console.log("showing breweries after likes added:",breweriesWithLikes);
-            parsedBody.data = breweriesWithLikes 
-            res.send(parsedBody);
-          })
+         var parsedBody = JSON.parse(body);
+         // console.log("showing results for nearby radius:", parsedBody.data);
+         // console.log()
+         res.send(parsedBody);
+         includeBreweryLikes(parsedBody.data)
+            .then(function(breweriesWithLikes) {
+               console.log("showing breweries after likes added:", breweriesWithLikes);
+               parsedBody.data = breweriesWithLikes
+               res.send(parsedBody);
+            })
 
          //console.log('/location: Sending Data')
-         // res.send(); 
+         // res.send();
       } else {
          console.log("/location error: ", error)
       }
    })
-}); 
-
-
+});
 
 app.post('/brewery/beer', function(req, res) {
 
@@ -193,119 +192,114 @@ app.post('/city', function(req, res) {
          console.log("/city error: ", error)
       }
    })
-}); 
+});
 
 //add like to database brewery table and brewery-likes join table if user has not previously liked the same brewery
 app.post('/brewery/like', function(req, res) {
 
-  console.log("got like request:", req.cookies);  
-  Session.findById(req.cookies.sessionId)
-    .then(function(data) {
-      console.log("returned from session.findById:", data); 
+   console.log("got like request:", req.cookies);
+   Session.findById(req.cookies.sessionId)
+      .then(function(data) {
+         console.log("returned from session.findById:", data);
 
-      var userId = data.user_id;
-      
-      Brewery.findById(req.body.breweryId)
-        .then(function(breweryData) {
+         var userId = data.user_id;
 
-          if(breweryData.length === 0) {
+         Brewery.findById(req.body.breweryId)
+            .then(function(breweryData) {
 
-            Brewery.create(req.body.breweryId, userId)
-            .then(function(data) { 
+               if (breweryData.length === 0) {
 
-              res.send(201, data[0]); 
+                  Brewery.create(req.body.breweryId, userId)
+                     .then(function(data) {
+
+                        res.send(201, data[0]);
+                     })
+               } else {
+                  Brewery.incrementLikes(req.body.breweryId, userId)
+                     .then(function(data) {
+                        if (!data[0]) {
+                           res.send(400, "error: user has already liked this brewery!")
+                        }
+                        res.send(201, data[0]);
+                     })
+               }
             })
-          }
-
-          else {
-            Brewery.incrementLikes(req.body.breweryId, userId)
-              .then(function(data) { 
-                if(!data[0]) {
-                  res.send(400, "error: user has already liked this brewery!")
-                }
-                res.send(201, data[0]);
-              })
-          }
-        }) 
-    })
-}) 
-
+      })
+})
 
 //retrieve user's previously liked breweries upon successful login
 app.post('/login', function(req, res) {
-  var username = req.body.username;
-  var password = req.body.password; 
+   var username = req.body.username;
+   var password = req.body.password;
 
-  console.log("inside logn endpoint:", username);
+   console.log("inside logn endpoint:", username);
 
-  User.findByUsername( username )
-    .then(function(user) {
-      console.log("showing user:", user);
-      if ( ! user ) {
-        res.send(400, 'user does not exist! please re-login or signup.');
-      }
-      else {
-        User.comparePassword( user.password, password )
-          .then(function (isMatch) {
+   User.findByUsername(username)
+      .then(function(user) {
+         console.log("showing user:", user);
+         if (!user) {
+            res.send(400, 'user does not exist! please re-login or signup.');
+         } else {
+            User.comparePassword(user.password, password)
+               .then(function(isMatch) {
 
-            if ( ! isMatch ) {
-              console.log("Incorrect password")
-              res.send(400, 'password is incorrect! please reenter');
+                  if (!isMatch) {
+                     console.log("Incorrect password")
+                     res.send(400, 'password is incorrect! please reenter');
 
-            } else {
-              Session.create( user.id )
-                .then(function (newSessionInfo) { 
-                  console.log("showing newSessionInfo:", newSessionInfo); 
-                  if(!newSessionInfo.id) {
-                    res.send(400, "user is already logged in!")
+                  } else {
+                     Session.create(user.id)
+                        .then(function(newSessionInfo) {
+                           console.log("showing newSessionInfo:", newSessionInfo);
+                           if (!newSessionInfo.id) {
+                              res.send(400, "user is already logged in!")
+                           } else {
+                              retrieveLikedBreweries(user.id)
+                                 .then(function(breweryData) {
+                                    console.log("showing brewery data:", breweryData);
+                                    res.cookie('sessionId', newSessionInfo.id)
+                                    res.send(201, breweryData);
+                                 })
+                           }
+                        })
                   }
-                  else {
-                    retrieveLikedBreweries(user.id)
-                      .then(function(breweryData) {
-                        console.log("showing brewery data:", breweryData);
-                        res.cookie('sessionId', newSessionInfo.id) 
-                        res.send(201, breweryData);
-                      }) 
-                  }
-                })
-            }
-          });
-      }
-    });
+               });
+         }
+      });
 });
 
-//Helper function called at the end of app.post('/login'). 
-//Upon a successful login it retrieves ids of the user's previously liked breweries and then 
-//does api calls for each one, returning an array of promised brewery data 
+//Helper function called at the end of app.post('/login').
+//Upon a successful login it retrieves ids of the user's previously liked breweries and then
+//does api calls for each one, returning an array of promised brewery data
 
 function retrieveLikedBreweries(userId) {
 
-  return Brewery.getLikedBreweries(userId)
-    .then(function(breweries) {
+   return Brewery.getLikedBreweries(userId)
+      .then(function(breweries) {
 
-      var promises = breweries.map(function(brewery, index) {
+         var promises = breweries.map(function(brewery, index) {
 
-        return new Promise(function(resolve, reject) {
+            return new Promise(function(resolve, reject) {
 
-          console.log("showing id:",breweries[index].id);
-          var url = 'http://api.brewerydb.com/v2/brewery/' + breweries[index].id + '/?key=da506aecce47e548b1877f8c6f9be793'
-        
-          request(url, function(error, response, body) {
+               console.log("showing id:", breweries[index].id);
+               var url = 'http://api.brewerydb.com/v2/brewery/' + breweries[index].id + '/?key=da506aecce47e548b1877f8c6f9be793'
 
-            if (!error && response.statusCode == 200) {
-             var data = JSON.parse(body); 
-             // console.log("showing likes inside new promise:", brewery.likes);
-             // console.log("data from api brewery id call:", data.data); 
-             data.data.likes = brewery.likes;
-             resolve(data.data);
-            }
-          })
-       
-        });
+               request(url, function(error, response, body) {
 
-      })  
-      return Promise.all(promises);
-    })
+                  if (!error && response.statusCode == 200) {
+                     var data = JSON.parse(body);
+                     // console.log("showing likes inside new promise:", brewery.likes);
+                     // console.log("data from api brewery id call:", data.data);
+                     data.data.likes = brewery.likes;
+                     resolve(data.data);
+                  }
+               })
+
+            });
+
+         })
+         return Promise.all(promises);
+      })
 }
 
 // function includeBreweryLikes(companies) {
@@ -317,7 +311,7 @@ function retrieveLikedBreweries(userId) {
 //     return new Promise(function(resolve, reject) {
 
 //       db('breweries').select('*').where('id', '=', company.brewery.id)
-//         .then(function(rows) { 
+//         .then(function(rows) {
 
 //           console.log("showing rows:", rows);
 
@@ -337,98 +331,89 @@ function retrieveLikedBreweries(userId) {
 
 function includeBreweryLikes(companies) {
 
-  // console.log("showing breweries:", companies);
+   // console.log("showing breweries:", companies);
 
-  var breweriesWithLikes = companies.map(function(company) {
+   var breweriesWithLikes = companies.map(function(company) {
 
-    return db('breweries').select('*').where('id', '=', company.brewery.id)
-      .then(function(rows) { 
+      return db('breweries').select('*').where('id', '=', company.brewery.id)
+         .then(function(rows) {
 
-        console.log("showing rows:", rows);
+            console.log("showing rows:", rows);
 
-        if(rows.length === 0) {
-          company.brewery.likes = 0;
-        }
-        else {
-          company.brewery.likes = rows[0].likes;
-        }
-        return company;
-      })
-  })
-  return Promise.all(breweriesWithLikes);
+            if (rows.length === 0) {
+               company.brewery.likes = 0;
+            } else {
+               company.brewery.likes = rows[0].likes;
+            }
+            return company;
+         })
+   })
+   return Promise.all(breweriesWithLikes);
 }
 
 app.get('/logout', function(req, res) {
-  Session.destroy( req.cookies.sessionId )
-    .then(function () {
-      res.clearCookie('sessionId');
-      res.redirect('/login');
-    })
-});
-
-app.post('/signup', function(req, res) { 
-  console.log("received request for sign up!");
-  var username = req.body.username;
-  var password = req.body.password;
-
-  User.findByUsername( username )
-    .then(function(user) { 
-      console.log("found by username:", username);
-      if ( user ) {
-        res.send(400, 'account already exists!');
-      }
-      else {
-        User.create({
-          username: username,
-          password: password
-        })
-          .then(function(newUserId) { 
-            console.log("inserted new user! :", newUserId);
-            return Session.create(newUserId);
-          })
-          .then(function (newSession) { 
-            console.log("showing next session:",newSession);
-            res.cookie('sessionId', newSession.id).sendStatus(201);
-          })
-      }
-    })
-});
-
-function getSignedInUser (req, res, next) {
-  var sessionId = req.cookies && req.cookies.sessionId
-
-  if ( ! sessionId ) {
-    res.redirect('/login');
-  }
-  else {
-    
-    Session.findById( sessionId )
-      .then(function (session) {
-        if ( ! session ) {
-          console.log("invalid session")
-          res.redirect('/login')
-        }
-        else {
-          return User.findById( session.user_id )
-            .then(function (user) {
-              if ( ! user ) {
-                console.log("invalid session (no such user)")
-                res.redirect('/login')
-              }
-              else {
-                req.user = user
-                next();
-              }
-            })
-        }
+   Session.destroy(req.cookies.sessionId)
+      .then(function() {
+         res.clearCookie('sessionId');
+         res.redirect('/login');
       })
-  }
-};
+});
 
+app.post('/signup', function(req, res) {
+   console.log("received request for sign up!");
+   var username = req.body.username;
+   var password = req.body.password;
+
+   User.findByUsername(username)
+      .then(function(user) {
+         console.log("found by username:", username);
+         if (user) {
+            res.send(400, 'account already exists!');
+         } else {
+            User.create({
+                  username: username,
+                  password: password
+               })
+               .then(function(newUserId) {
+                  console.log("inserted new user! :", newUserId);
+                  return Session.create(newUserId);
+               })
+               .then(function(newSession) {
+                  console.log("showing next session:", newSession);
+                  res.cookie('sessionId', newSession.id).sendStatus(201);
+               })
+         }
+      })
+});
+
+function getSignedInUser(req, res, next) {
+   var sessionId = req.cookies && req.cookies.sessionId
+
+   if (!sessionId) {
+      res.redirect('/login');
+   } else {
+
+      Session.findById(sessionId)
+         .then(function(session) {
+            if (!session) {
+               console.log("invalid session")
+               res.redirect('/login')
+            } else {
+               return User.findById(session.user_id)
+                  .then(function(user) {
+                     if (!user) {
+                        console.log("invalid session (no such user)")
+                        res.redirect('/login')
+                     } else {
+                        req.user = user
+                        next();
+                     }
+                  })
+            }
+         })
+   }
+};
 
 var port = process.env.PORT || 1337;
 app.listen(port);
 console.log("Listening on localhost:" + port);
-
-
-
